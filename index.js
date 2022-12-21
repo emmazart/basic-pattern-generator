e4cfd5d95f1620aaeceda2f9093cac4b1d98a645
Emma Olson <emma.olson794@gmail.com>
2022-12-21 12:22:46 -0600

    working on hat math function

--- index.js
+++ index.js
@@ -21,15 +21,21 @@ const stsPerInch = {
     'Super Bulky[6]': 2
 };
 
-const math = function(makeItem, gauge) {
-        
+//--------------------- MATH FUNCTION TAKES IN INQUIRER RESPONSE AND PASSES INFO TO SWITCH CASE, DETERMINING BASIC PATTERN INFO -------------- //
+const math = function(makeItem, gauge, ...params) {
     let patternInfo = {};
 
     switch(makeItem) {
         case 'Scarf':
-            let castOn = 8*gauge;
-            let rows = 60*gauge;
-            patternInfo = {...patternInfo, "castOn": castOn, "rows": rows};
+            patternInfo = {...patternInfo, "castOn": 8*gauge, "rows": 60*gauge};
+            break;
+        case 'Adult Hat': 
+            patternInfo = {...patternInfo, "castOn": 20*gauge, "rows": 8*gauge};
+            break;
+        case 'Use my own measurements':
+            let width = params[0];
+            let length = params[1];
+            patternInfo = {...patternInfo, "castOn": width*gauge, "rows": length*gauge};
             break;
         default:
             console.log("Error, please choose a valid item to make");
@@ -38,13 +44,58 @@ const math = function(makeItem, gauge) {
     return patternInfo;
 };
 
+//-------------------- DECREASE FUNCTION DETERMINES DECREASES IN HAT PATTERN -------------------------//
+const decreases = function(sts) {
+    let numOfDecrease = 6;
+    let freqOfDecrease = sts/numOfDecrease;
+    // k10(1-10), k2tog(11/12), k10(13-22), k2tog(23/24), k10(34), k2tog(34/35), k10(45), k2tog(45/46), k10(56), k2tog(57/58), k2(60) 
+    let rowDec = numOfDecrease-1;
+    let remainder = (sts-rowDec)-(freqOfDecrease*rowDec);
+    const decString = `(Knit ${freqOfDecrease} stitches, k2tog) ${numOfDecrease} times. K ${remainder} sts. Repeat until you have ${numOfDecrease-1} sts left. Pass yarn through remaining live stitches and fasten off.`;
+
+    let info = {
+        'remainder': remainder,
+        'numOfDecrease': numOfDecrease,
+        'freqOfDecrease': freqOfDecrease,
+        'rowDec': rowDec
+    }
+    console.log(info);
+}
+
+console.log(decreases(60));
 
 // scarf measurements = 8x60
 // console.log(stsPerFourInch["Lace[0]"]/4)
 
-inquirer.prompt(questions).then((answer) => {
-    let { yarnWeight, makeItem, craft } = answer; // destructure answer object
-    let gauge = stsPerInch[yarnWeight]; // find the gauge associated with chosen yarnWeight
-    let patternInfo = math(makeItem, gauge);
-    console.log(`You are ${craft}ing a ${makeItem} in ${yarnWeight} weight yarn. Cast on ${patternInfo.castOn} stitches and work for ${patternInfo.rows} rows.`);
-});
+// inquirer.prompt(questions).then((answer) => {
+//     console.log(answer);
+//     let { yarnWeight, makeItem, craft } = answer; // destructure answer object
+
+//     // determine which way we are figuring out gauge:
+//     let gauge;
+//     if (yarnWeight) {gauge = stsPerInch[yarnWeight];} // find the gauge associated with chosen yarnWeight
+//     else {
+//         gauge = answer.userStsPerInch
+//     }
+//     // if (answer.userStsPerInch && answer.userRowsPerInch) {gauge = answer.userStsPerInch};
+
+//     let patternInfo;
+//     if (answer.userWidth && answer.userLength) {
+//         patternInfo = math(makeItem, gauge, answer.userWidth, answer.userLength)
+//     } else {
+//         patternInfo = math(makeItem, gauge);
+//         if (makeItem === 'Adult Hat') {
+//             // do some math to figure out decreases and add it to the pattern info
+//             patternInfo = {...patternInfo, "decreases": decreases(patternInfo.castOn)};
+//         }
+//     }
+
+//     console.log(patternInfo);
+
+//     // conditional output based on how we are getting gauge
+//     if (yarnWeight) {
+//         console.log(`You are ${craft}ing a ${makeItem} in ${yarnWeight} weight yarn. Cast on ${patternInfo.castOn} stitches and work for ${patternInfo.rows} rows.`);     
+//     } else {
+//         console.log(`You are ${craft}ing a ${makeItem} at gauge ${answer.userStsPerInch} stitches per inch. Cast on ${patternInfo.castOn} stitches and work for ${patternInfo.rows} rows.`);     
+//     }
+// });
--- prompts.js
+++ prompts.js
@@ -1,20 +1,86 @@
 module.exports = [
     {
+        // determines how we get gauge for pattern info
+        name: 'confirmGauge',
+        type: 'rawlist',
+        message: "Would you like to input your gauge or select a yarn weight?",
+        choices: ['Use my own gauge', 'Select a yarn weight'],
+        // default: 1,
+    },
+    {
+        // if user does not have their own gauge, this question will run
         type: 'rawlist',
         choices: ['Lace[0]', 'Fingering[1]', 'Sport[2]', 'DK[3]', 'Worsted[4]', 'Bulky[5]', 'Super Bulky[6]'],
         message: "What weight of yarn would you like to use?",
         name: 'yarnWeight',
+        when: ({ confirmGauge }) => {
+            if (confirmGauge === 'Select a yarn weight') {
+                return true;
+            } else {
+                return false;
+            }
+        } 
+    },
+    {
+        // if user DOES have their own gauge, this question will run
+        type: 'number',
+        message: "How many stitches per inch?",
+        name: 'userStsPerInch',
+        when: ({ confirmGauge }) => {
+            if (confirmGauge === 'Use my own gauge') {
+                return true;
+            } else {
+                return false;
+            }
+        } 
+    },
+    {
+        // if user DOES have their own gauge, this question will run
+        type: 'number',
+        message: "How many rows per inch?",
+        name: 'userRowsPerInch',
+        when: ({ confirmGauge }) => {
+            if (confirmGauge === 'Use my own gauge') {
+                return true;
+            } else {
+                return false;
+            }
+        } 
     },
     {
         type: 'rawlist',
-        choices: ["Adult Hat", 'Scarf'],
+        choices: ["Adult Hat", 'Scarf', 'Use my own measurements'],
         message: "What would you like to make?",
         name: "makeItem"
+    },
+    {
+        type: 'number',
+        message: 'Enter the desired width (inches):',
+        name: 'userWidth',
+        when: ({ makeItem }) => {
+            if (makeItem === 'Use my own measurements') {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    },
+    {
+        type: 'number',
+        message: 'Enter the desired length (inches):',
+        name: 'userLength',
+        when: ({ makeItem }) => {
+            if (makeItem === 'Use my own measurements') {
+                return true;
+            } else {
+                return false;
+            }
+        }
     },
     {
         type: 'rawlist',
         choices: ["Knit", "Crochet"],
         message: "Are you knitting or crocheting?",
         name: 'craft'
-    }
+    },
 ];
